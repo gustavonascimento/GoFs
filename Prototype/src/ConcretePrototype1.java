9775255e5c0c08d272a9429467f70f2102e54507
RafaelRabetti <leafar_rabetti@hotmail.com>
2016-11-17 00:57:25 -0200

    adding a simple implementation of prototype pattern

---
+++ Prototype/src/ConcretePrototype1.java
@@ -0,0 +1,8 @@
+
+public class ConcretePrototype1 extends Prototype{
+
+	@Override
+	public Prototype clone() throws CloneNotSupportedException{
+		return super.clone();
+	}
+}
---
+++ Prototype/src/ConcretePrototype2.java
@@ -0,0 +1,8 @@
+
+public class ConcretePrototype2 extends Prototype {
+
+	@Override
+	public Prototype clone() throws CloneNotSupportedException{
+		return super.clone();
+	}
+}
